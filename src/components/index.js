83dc9e3906d133bfdb5eef1bde68e1c5e41fc4ce
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-10-03 22:06:16 +0530

    added hero section

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
 import Layout from "./layout/Layout";
+import HeroSection from "./heroSection/HeroSection";
 
-export { Navbar, Footer, Layout };
+export { Navbar, Footer, Layout, HeroSection };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
-import { Layout } from "../../components";
+import { HeroSection, Layout } from "../../components";
 import MyContext from "../../context/myContext";
 
 const Home = () => {
   return (
     <Layout>
-      Home
+      <HeroSection />
     </Layout>
   );
 };
