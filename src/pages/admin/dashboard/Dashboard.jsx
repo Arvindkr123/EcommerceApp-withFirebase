1d5a7a0c18188849c96c6b655f3e11d599112a5a
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-10-04 16:09:07 +0530

    added dashboard tab

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import MyContext from '../../../context/myContext'
 import { Layout } from '../../../components';
 import { FaUserTie } from 'react-icons/fa';
+import DashboardTab from './DashboardTab';
 
 const Dashboard = () => {
   const { mode } = useContext(MyContext);
@@ -9,6 +10,7 @@ const Dashboard = () => {
     <Layout>
       <section className="text-gray-600 body-font mt-10 mb-10">
         <div className="container px-5 mx-auto mb-10">
+
           <div className="flex flex-wrap -m-4 text-center">
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className=" border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
@@ -39,6 +41,7 @@ const Dashboard = () => {
                 <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total Users</p>
               </div>
             </div>
+
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className=" border-2 hover:shadow-purple-600 shadow-[inset_0_0_10px_rgba(0,0,0,0.6)] bg-gray-100 border-gray-300    px-4 py-3 rounded-xl" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                 <div className="text-purple-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
@@ -48,9 +51,13 @@ const Dashboard = () => {
                 <p className=" text-purple-500  font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total Products</p>
               </div>
             </div>
+
+            
           </div>
+          <DashboardTab />
         </div>
       </section>
+
     </Layout>
   )
 }
